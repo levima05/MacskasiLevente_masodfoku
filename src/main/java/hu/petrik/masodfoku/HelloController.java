bdde2765dfbd32e8dd96e164d7625cd7407989a5
Petrik Diak <diak@petrik.hu>
2024-12-13 15:34:11 +0100

    Done

--- src/main/java/hu/petrik/masodfoku/HelloController.java
+++ src/main/java/hu/petrik/masodfoku/HelloController.java
@@ -2,13 +2,59 @@ package hu.petrik.masodfoku;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField aField;
+    @FXML
+    private TextField bField;
+    @FXML
+    private TextField cField;
+    @FXML
+    private Label x1Label;
+    @FXML
+    private Label x2Label;
+    @FXML
+    private Label errorLabel;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void calculateRoots() {
+        x1Label.setText("x1 = ");
+        x2Label.setText("x2 = ");
+        errorLabel.setText("");
+
+        try {
+            double a = Double.parseDouble(aField.getText());
+            double b = Double.parseDouble(bField.getText());
+            double c = Double.parseDouble(cField.getText());
+
+            if (aField.getText().isEmpty() || bField.getText().isEmpty() || cField.getText().isEmpty()) {
+                errorLabel.setText("Minden mező kitöltése kötelező");
+                return;
+            }
+
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant < 0) {
+                errorLabel.setText("Nem oldható meg a valós számok halmazán");
+            } else if (discriminant == 0) {
+                double x1 = -b / (2 * a);
+                x1Label.setText("x1 = " + x1);
+                x2Label.setText("x2 = " + x1);
+            } else {
+                double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+                double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+                x1Label.setText("x1 = " + x1);
+                x2Label.setText("x2 = " + x2);
+            }
+
+        } catch (NumberFormatException e) {
+            errorLabel.setText("Csak számot adhat meg");
+        } catch (Exception e) {
+            errorLabel.setText("Hiba történt a számítás közben");
+        }
     }
 }
